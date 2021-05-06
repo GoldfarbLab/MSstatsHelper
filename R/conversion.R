7c9fac2a6611af89f662b3ba038268ab1f386d30
Emily King <e.a.king@wustl.edu>
2021-05-06 16:20:27 -0500

    Added gene map and updated conversion function

--- R/conversion.R
+++ R/conversion.R
@@ -26,10 +26,42 @@ sortMultiColumn <- function(x) {
   sapply(str_split(x, ";"), function(p) str_c(sort(p), collapse=";"))
 }
 
+update.gene.names <- function(df) {
+  map <- invisible(genes.map)
+
+  df <- df %>%
+    mutate(pid = row_number(), listed_proteins = str_split(`Proteins`, ";")) %>%
+    unnest(listed_proteins) %>%
+    mutate(listed_proteins = sapply(str_split(listed_proteins, "-"), `[[`, 1))
+  df <- distinct(df)
+
+
+  df <- df %>% left_join(map, by = c("listed_proteins" = "Entry"))
+
+  converted.list <- df %>% group_by(pid) %>%
+    summarize(`Gene names` = str_c(unique(`Gene names`), collapse = ';'))
+
+  df <- df %>%
+    left_join(converted.list, by = c("pid")) %>%
+    rename(`Gene names` = `Gene names.y`) %>%
+    select(-pid, -listed_proteins, -`Gene names.x`, - gene.name)
+
+  return(distinct(df))
+}
+
 prepareForMSstats <- function(phosphosites, global_evidence, min_match=2, min_global_nonMissing=2) {
+  phosphosites <- localized.sites
+  global_evidence <- evidence
+
+  phosphosites <- update.gene.names(phosphosites)
+  global_evidence <- update.gene.names(global_evidence)
+
+  # order protein IDs and gene names
+  global_evidence <- sortIdentifiers(global_evidence)
+  phosphosites <- sortIdentifiers(phosphosites)
+
   # filter out rows with too many missing values
   global_evidence <- global_evidence %>% filter(rowSums(across(matches("Reporter intensity corrected \\d+"), function(x) x > 0)) >= min_global_nonMissing)
-
   # find phosphosites with enough distinct peptides in the global data that have exact protein matches
   matching_phospho_protein <- phosphosites %>%
     inner_join(global_evidence, by = c("Sorted Proteins")) %>%
@@ -49,6 +81,17 @@ prepareForMSstats <- function(phosphosites, global_evidence, min_match=2, min_gl
     group_by(id.x, `Sorted Gene names`) %>%
     summarize(num_matches = n())
 
+  not_matching_phospho <- phosphosites %>%
+    filter(!id %in% matching_phospho_protein$id.x) %>%
+    filter(!id %in% matching_phospho_gene$id.x) %>%
+    select(id, Proteins, `Gene names`) %>%
+    rename("id.x" = id,
+           "Sorted Proteins" = Proteins,
+           "Sorted Gene names" = "Gene names") %>%
+    mutate(`Sorted Gene names` = str_split(`Sorted Gene names`, ";")) %>%
+    unnest(`Sorted Gene names`)
+
+
   # create new identifiers for global for MSstats
   # evidence that matches proteins exactly
   global_evidence_protein <- global_evidence %>%
@@ -64,7 +107,7 @@ prepareForMSstats <- function(phosphosites, global_evidence, min_match=2, min_gl
            !`Sorted Gene names` %in% matching_phospho_gene$`Sorted Gene names`) %>%
     mutate("matched_ids" =  `Sorted Proteins`)
   # merge all (doesn't include unquantified evidence)
-  global_evidence <- bind_rows(global_evidence_gene, global_evidence_protein, global_evidence_gene)
+  global_evidence <- bind_rows(global_evidence_gene, global_evidence_protein, global_evidence_no_match)
 
   # create new identifier columns for phospho for MSstats
   phosphosites_protein <- phosphosites %>%
@@ -77,7 +120,12 @@ prepareForMSstats <- function(phosphosites, global_evidence, min_match=2, min_gl
     mutate("matched_ids" = addSitesToGeneNames(`Gene names`, Proteins, `Positions within proteins`, `Amino acid`)) %>%
     mutate("matched_ids" = sortMultiColumn(matched_ids))
 
-  phosphosites <- bind_rows(phosphosites_genes, phosphosites_protein)
+  phosphosites_no_match <- phosphosites %>%
+    filter(id %in% not_matching_phospho$id.x) %>%
+    mutate("matched_ids" = addSitesToGeneNames(`Gene names`, Proteins, `Positions within proteins`, `Amino acid`)) %>%
+    mutate("matched_ids" = sortMultiColumn(matched_ids))
+
+  phosphosites <- bind_rows(phosphosites_genes, phosphosites_protein, phosphosites_no_match)
 
   return(list(global_evidence, phosphosites))
 
@@ -95,27 +143,54 @@ addSitesToNames <- function(ids, positions, amino_acid) {
 }
 
 addSitesToGeneNames <- function(genes, ids, positions, amino_acid) {
-  split_genes = str_split(genes, ";")
-  split_ids = str_split(ids, ";")
-  split_positions = str_split(positions, ";")
+  map <- invisible(genes.map)
+  phos_genes <- bind_cols(list(genes, ids, positions, amino_acid)) %>%
+    rename(genes = "...1", Proteins = "...2", Positions = "...3", aa = "...4")
 
-  ids_with_sites <- mapply(function(s_genes, s_ids, s_pos, aa) {
-    unique_ids <- unique(sapply(s_ids, function(x) str_replace(x, "-.*", "")))
+  phos_genes <- phos_genes %>%
+    mutate(rid = row_number()) %>%
+    mutate(Positions = str_split(Positions, ';'),
+           Proteins = str_split(Proteins, ";")) %>%
+    unnest(c(Positions, Proteins))
 
-    unique_indices = sapply(unique_ids, function(x) min(which(str_detect(s_ids, x))))
+  phos_genes <- phos_genes %>%
+    mutate(Entry = sapply(str_split(Proteins, "-"), `[[`, 1))
 
-    #if (length(s_genes) != length(unique_indices)) {
-    #  print(s_genes)
-    #  print(s_ids)
-    #  print(unique_ids)
-    #  print(unique_indices)
-    #}
+  phos_genes <- phos_genes %>% left_join(map, by = c("Entry")) %>%
+    mutate(ids_with_sites = str_c(gene.name,"_",
+                                  Proteins,"_",
+                                  aa, Positions))
 
-    str_c(s_genes, "_", s_ids[unique_indices], "_", aa, s_pos[unique_indices], collapse=";")
-  }, s_genes=split_genes, s_ids=split_ids, s_pos=split_positions, aa=amino_acid)
+  phos_genes <- phos_genes %>%
+    group_by(rid) %>%
+    summarize(ids_with_sites = str_c(ids_with_sites, collapse = ';'))
 
-  return(ids_with_sites)
+  return(phos_genes$ids_with_sites)
 }
+
+
+# addSitesToGeneNames <- function(genes, ids, positions, amino_acid) {
+#   split_genes = str_split(genes, ";")
+#   split_ids = str_split(ids, ";")
+#   split_positions = str_split(positions, ";")
+#
+#   ids_with_sites <- mapply(function(s_genes, s_ids, s_pos, aa) {
+#     unique_ids <- unique(sapply(s_ids, function(x) str_replace(x, "-.*", "")))
+#
+#     unique_indices = sapply(unique_ids, function(x) min(which(str_detect(s_ids, x))))
+#
+#     #if (length(s_genes) != length(unique_indices)) {
+#     #  print(s_genes)
+#     #  print(s_ids)
+#     #  print(unique_ids)
+#     #  print(unique_indices)
+#     #}
+#
+#     str_c(s_genes, "_", s_ids[unique_indices], "_", aa, s_pos[unique_indices], collapse=";")
+#   }, s_genes=split_genes, s_ids=split_ids, s_pos=split_positions, aa=amino_acid)
+#
+#   return(ids_with_sites)
+# }
 
 
 
